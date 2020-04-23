0ec44efe27ed439d68fe6c51318ec6f1f7006a64
lopez-isaac <lopez-isaac@users.noreply.github.com>
2020-04-23 19:02:14 -0400

    day 4

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,10 +1,11 @@
 import sys
+from collections import deque
 sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
 
-class BinarySearchTreeNode:
+class BinarySearchTree:
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -21,57 +22,94 @@ class BinarySearchTreeNode:
             # the left side is empty
             else:
                 # we've found a valid parking spot
-                self.left = BinarySearchTreeNode(value)
+                self.left = BinarySearchTree(value)
         # otherwise, value >= self.value
         else:
             if self.right:
                 self.right.insert(value)
             else:
-                self.right = BinarySearchTreeNode(value)
+                self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        if self.value == None:
-            return False
         if self.value == target:
             return True
         else:
             if target < self.value:
+                if not self.left:
+                    return False
                 #find on left node
-                self.left = BinarySearchTreeNode(target)
+                return self.left.contains(target)
             else:
+                if not self.right:
+                    return False
                 #find on right node
-                self.right = BinarySearchTreeNode(target)
+                return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
         if self.right:
-            self.right = get_max()
+            return self.right.get_max()
         else:
             return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        # in-order traversal results in sorted list. left, root, right:
+        # start at left node and go through subtrees, root, lastly right side
+        if node:
+            if node.left:
+                self.in_order_print(node.left)
+            print(node.value)
+
+            if node.right:
+                self.in_order_print(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        q = deque()
+        q.append(self)
+        while len(q) > 0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+            print(current_node.value)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = []
+        current_node = node
+        # add root to the stack
+        stack.append(current_node)
+        while len(stack) > 0:
+            current_node = stack.pop()
+            # check if the right child exists
+            if current_node.right:
+                stack.append(current_node.right)
+            # check if the left child exists
+            if current_node.left:
+                stack.append(current_node.left)
+            print(current_node.value)
+
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
@@ -83,3 +121,6 @@ class BinarySearchTreeNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+
